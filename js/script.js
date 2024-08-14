951037a7c3db6f08755e4ce0c7b5d1a9b0289c79
Salome Durand <salome.durand.adeline4@hotmail.fr>
2024-08-14 17:09:06 +0200

    Suppresion du Jquery par du Javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-$(document).ready(function () {
+//document.addEventListener("DOMContentLoaded", function (){
 
+        // rejouer le jeu
+//const replay = document.getElementById("replay");
+//replay.addEventListener("click", restart);
+//        function restart (){}
+
+        
     // générer un nombre au hasard
     const randomNumber = Math.floor(Math.random() * 10)
 
-    // pour que le formulaire puisse être détecté après entrée
-    const formUser = document.getElementById("formUser");
-    $("#formUser").on("submit", function (e) {
-        e.preventDefault()
-        const input = $("input[type=text]")
+console.log(randomNumber)
 
-        const body = $("body")
+    const success = document.getElementById("check")
+    const replay = document.getElementById("replay")
+
+    // pour que le formulaire puisse être détecté après entrée
+    const formUser = document.getElementById("formUser")
+    formUser.addEventListener("submit", function (event) {
+        event.preventDefault()
+        const input = document.getElementById("numberUser")
+
+        const body = document.querySelector("body")
 
     // jouer effets sonores
         const audioError = document.getElementById("audioError");
@@ -22,25 +33,25 @@ $(document).ready(function () {
             }
 
         // détecter la valeur du formulaire    
-        const userValue = parseInt($(input).val())
+        const userValue = parseInt(input.value)
 
         // comparaison du nombre généré et du nombre choisi par le joueur
         if (randomNumber !== userValue) {
-            input.val("")          // réinitialiser la valeur de l'input sans supprimer la valeur précédemment entrée
-            input.addClass("animate__animated animate__bounce")   // ajouter un effet lors de l'erreur
+            input.value=""           // réinitialiser la valeur de l'input sans supprimer la valeur précédemment entrée
+            input.classList.add("animate__animated", "animate__bounce")   // ajouter un effet lors de l'erreur
             setTimeout(function () {                                // enlever l'effet pour qu'il puisse se répéter
-                input.removeClass("animate__animated animate__bounce")
+                input.classList.remove("animate__animated", "animate__bounce")
             }, 1000)
             playAudioError()
-            body.addClass("error") // changer la couleur de l'élément lorsque l'utilisateur a faux
+            body.classList.add("error") // changer la couleur de l'élément lorsque l'utilisateur a faux
         }
-        // faire disparaître l'input et appraître l'icône succès avec animation
+        // faire disparaître l'input et appraître l'icône succès avec animation + replay
         else {
             input.remove()
             playAudioSuccess()
-            $(".fa-circle-check").show()
-            $(".fa-circle-check").addClass("animate__animated animate__heartBeat")
-            body.removeClass("error")
+            success.style.display = "block";
+            success.classList.add("animate__animated", "animate__heartBeat")
+            replay.style.display = "block";
+            body.classList.remove("error")
         }
     })
-})
