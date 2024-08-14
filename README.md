8de7a99d737e2c8e409b310d0bf3e44be24d711f
Salome Durand <salome.durand.adeline4@hotmail.fr>
2024-08-14 14:57:57 +0200

    Ajout d'effets sonores pendant les animations.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mon beau projet
-[tutoriel (de base)](https://www.youtube.com/watch?v=S4WQy94ybtw)
+# projet jeux
+[tutoriel de base pour chiffre au hasard](https://www.youtube.com/watch?v=S4WQy94ybtw)
--- js/script.js
+++ js/script.js
@@ -4,11 +4,22 @@ $(document).ready(function () {
     const randomNumber = Math.floor(Math.random() * 10)
 
     // pour que le formulaire puisse être détecté après entrée
+    const formUser = document.getElementById("formUser");
     $("#formUser").on("submit", function (e) {
         e.preventDefault()
         const input = $("input[type=text]")
 
         const body = $("body")
+
+    // jouer effets sonores
+        const audioError = document.getElementById("audioError");
+        const audioSuccess = document.getElementById("audioSuccess");
+        function playAudioError() { 
+            audioError.play(); 
+            }
+        function playAudioSuccess() { 
+            audioSuccess.play(); 
+            }
 
         // détecter la valeur du formulaire    
         const userValue = parseInt($(input).val())
@@ -20,12 +31,13 @@ $(document).ready(function () {
             setTimeout(function () {                                // enlever l'effet pour qu'il puisse se répéter
                 input.removeClass("animate__animated animate__bounce")
             }, 1000)
-
+            playAudioError()
             body.addClass("error") // changer la couleur de l'élément lorsque l'utilisateur a faux
         }
         // faire disparaître l'input et appraître l'icône succès avec animation
         else {
             input.remove()
+            playAudioSuccess()
             $(".fa-circle-check").show()
             $(".fa-circle-check").addClass("animate__animated animate__heartBeat")
             body.removeClass("error")
